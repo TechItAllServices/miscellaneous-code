e21cd77dd3a88cd87ba3e24ad9d9a71cc1140379
TechItAll <128874430+TechItAll@users.noreply.github.com>
2023-10-29 14:40:38 -0400

    Create basicmovement.cs

---
+++ basicmovement.cs
@@ -0,0 +1,18 @@
+// NOTE: Not tested. Report any issues here: https://github.com/TechItAllServices/miscellaneous-code/issues
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
+
