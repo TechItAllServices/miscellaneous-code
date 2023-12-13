d5aa44d88823693103fccdcc0d6c9a47cf4034cc
TechItAllSA <145471910+TechItAllSA@users.noreply.github.com>
2023-12-13 13:25:04 +0000

    Added commin.py but not done! idk about cal.js

---
+++ commin.py
@@ -0,0 +1,8 @@
+# If you can read this this does nothing. Its for show!
+import time,os
+import colorama
+
+print("Loading Configs...",end="\r")
+time.sleep(2)
+print(f"{colorama.Fore.GREEN}Loading Configs...Ok{colorama.Fore.RESET}")
+# Note: Program is not done!
