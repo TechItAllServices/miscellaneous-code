db6fe61dad294be7fb5d135ebce96b3c8a41096e
TechItAllSA <145471910+TechItAllSA@users.noreply.github.com>
2024-01-08 13:04:23 -0500

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,1 +1,36 @@
+import time
+import  threading
+from pynput.mouse import Button, Controller
+from pynput.keybord import listener, keycode
+ 
 
+delay = 0.001
+button = button.left
+start_stop_key = keycode(char=’s’)
+exit_key = keycode(char=’e’)
+
+class ClickMouse(threading.Thread):
+	def __init__(self, delay, button):
+		super(ClickMouse, self) .__init__()
+		self.delay = delay
+		self.button = button
+		self.running = false
+		self.program_running = true
+
+def  start_clicking(self):
+	self.running = true
+
+def stop_clicking(self):
+	self.running = false
+
+def exit(self):
+	self.stop_clicking()
+	self.program_running = false
+
+def run(self):
+	while self.program_running:
+		while self.running:
+			mouse.click(self.button)
+		time.sleep(0.1)
+
+mouse
